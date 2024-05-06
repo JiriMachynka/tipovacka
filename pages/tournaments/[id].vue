654304a2886644397617dc63f3897d296908ef92
Jiří Machynka <machynka.j@gmail.com>
2024-05-06 12:32:09 +0200

    feat: error page and middleware

--- pages/tournaments/[id].vue
+++ pages/tournaments/[id].vue
@@ -1,3 +1,19 @@
+<script lang="ts" setup>
+const { $client } = useNuxtApp();
+
+const route = useRoute();
+
+const tournamentId = +route.params.id;
+
+const { data: tournament } = await $client.tournament.getData.useQuery({ tournamentId });
+
+if (!tournament.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Tipovačka nebyla nalezena',
+  })
+}
+</script>
 <template>
   <NuxtLayout>
     <NuxtPage />
