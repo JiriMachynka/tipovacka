704e8b189d7b87552979ae9dff7e15dd068d4eb2
Jiří Machynka <machynka.j@gmail.com>
2024-05-09 18:48:48 +0200

    feat: render button as child

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="flex flex-col justify-center items-center h-[100dvh]">
-    <Button
-      class="text-4xl px-7 py-5"
-      @click="navigateTo('/login')"
-    >
-      Přihlásit se
+    <Button class="text-4xl px-7 py-5" as-child>
+      <NuxtLink to="/login">
+        Přihlásit se
+      </NuxtLink>
     </Button>
   </div>
 </template>
