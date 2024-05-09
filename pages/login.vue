5825b2fe66028755c86d997b96a38c9f65cf4641
Jiří Machynka <machynka.j@gmail.com>
2024-05-09 12:37:27 +0200

    refactor: login page

--- pages/login.vue
+++ pages/login.vue
@@ -13,12 +13,7 @@ const validationSchema = toTypedSchema(
 	}),
 );
 
-const { defineField, handleSubmit, isSubmitting, resetForm } = useForm({
-	validationSchema,
-});
-
-const [email, emailAttrs] = defineField('email');
-const [password, passwordAttrs] = defineField('password');
+const { handleSubmit, isSubmitting, resetForm } = useForm({ validationSchema });
 
 const onSubmit = handleSubmit(async (values) => {
 	const { error } = await supabaseClient.auth.signInWithPassword({
@@ -46,18 +41,34 @@ const onSubmit = handleSubmit(async (values) => {
 });
 </script>
 <template>
-  <main class="containter flex flex-col gap-4 justify-center items-center h-[100dvh]">
+  <main class="containter flex flex-col gap-4 justify-center items-center h-dvh px-6 lg:px-0">
     <form class="flex flex-col gap-2 max-w-3xl w-full" @submit="onSubmit">
-      <h1 class="text-3xl font-bold text-center">Přihlášení</h1>
-      <div class="flex flex-col gap-4">
-        <Label html="email">Email</Label>
-        <Input id="email" v-model="email" v-bind="emailAttrs" />
-      </div>
-      <div class="flex flex-col gap-4">
-        <Label html="password">Heslo</Label>
-        <Input id="password" type="password" v-model="password" v-bind="passwordAttrs" autocomplete="off" />
-      </div>
-      <Button type="submit" :disabled="isSubmitting">Přihlásit se</Button>
+      <h1 class="text-4xl font-bold text-center">Přihlášení</h1>
+			<FormField v-slot="{ componentField }" name="email">
+				<FormItem>
+					<FormLabel>Email</FormLabel>
+					<FormControl>
+						<Input type="text" v-bind="componentField" />
+					</FormControl>
+					<FormMessage />
+				</FormItem>
+			</FormField>
+			<FormField v-slot="{ componentField }" name="password">
+				<FormItem>
+					<FormLabel>Heslo</FormLabel>
+					<FormControl>
+						<Input type="password" v-bind="componentField" />
+					</FormControl>
+					<FormMessage />
+				</FormItem>
+			</FormField>
+      <Button
+				type="submit"
+				class="text-xl font-semibold"
+				:disabled="isSubmitting"
+			>
+				Přihlásit se
+			</Button>
     </form>
     <div class="flex flex-col items-center text-lg">
       <p class="font-semibold">
