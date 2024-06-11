0028873bb83cab95e73688c23fbcadace7417450
Jiří Machynka <machynka.j@gmail.com>
2024-06-11 18:23:53 +0200

    feat: better looking main page & scrollbar update

--- pages/tournaments/index.vue
+++ pages/tournaments/index.vue
@@ -24,20 +24,22 @@ const logout = async () => {
     <Button class="my-3" @click="logout">
       <LogOut class="size-8" />
     </Button>
-    <NuxtLink
-      v-if="tournaments"
-      v-for="tournament in tournaments"
-      :key="tournament.id"
-      :to="`/tournaments/${tournament.id}`"
-      :class="cn('flex items-center justify-between w-full p-5 text-xl rounded-lg text-slate-50 py-10 group bg-gradient-to-r from-slate-900 to-slate-800 hover:text-slate-50 border-2 border-slate-50 transition-all duration-700 mb-2',
-        tournaments.length > 1 && 'my-2' 
-      )"
-    >
-      <span>{{ tournament.name }}</span>
-      <div class="mr-5 group-hover:mr-0 transition-all ease-in-out duration-300">
-        <ChevronRight class="size-6" /> 
-      </div>
-    </NuxtLink>
+    <div class="flex flex-col gap-4 w-full overflow-y-auto max-h-[70dvh]">
+      <NuxtLink
+        v-if="tournaments"
+        v-for="tournament in tournaments"
+        :key="tournament.id"
+        :to="`/tournaments/${tournament.id}`"
+        :class="cn('flex items-center justify-between w-full p-5 text-xl rounded-lg text-slate-50 py-10 group bg-gradient-to-r from-slate-900 to-slate-800 hover:text-slate-50 border-2 border-slate-50 transition-all duration-700 mb-2',
+          tournaments.length > 1 && 'my-2' 
+        )"
+      >
+        <span>{{ tournament.name }}</span>
+        <div class="mr-5 group-hover:mr-0 transition-all ease-in-out duration-300">
+          <ChevronRight class="size-6" /> 
+        </div>
+      </NuxtLink>
+    </div>
     <Button
       :class="cn('text-2xl py-4 w-full',
         tournaments?.length === 0 && 'text-3xl py-7 rounded-xl' 
