0b008e1e498a92c5641553733380764f9f76099c
Jiří Machynka <machynka.j@gmail.com>
2024-06-15 17:40:23 +0200

    feat: update border on main table & change border color from slate-50 to primary to adapt on dark/light mode

--- pages/tournaments/[id]/index.vue
+++ pages/tournaments/[id]/index.vue
@@ -18,46 +18,46 @@ const numberOfMatches = tournament.value?.userMatches ? tournament.value?.userMa
     <p class="text-3xl font-bold text-center md:text-left">Ještě nebyly vyhodnoceny žádné zápasy</p>
   </div>
   <div v-else class="flex justify-center mt-5">
-    <div class="flex flex-col border border-slate-50">
-      <div class="border-b border-slate-50 flex flex-col-reverse lg:flex-row items-center lg:items-start p-0 lg:p-3 lg:gap-2 justify-center">
+    <div class="flex flex-col border border-primary">
+      <div class="border-b border-primary flex flex-col-reverse lg:flex-row items-center lg:items-start p-0 lg:p-3 lg:gap-2 justify-center">
         <span class="flex justify-center w-full p-2 lg:p-0"><IconUsers /></span>
         <span class="hidden lg:inline">/</span>
-        <span class="flex justify-center w-full p-2 lg:p-0 border-b border-b-slate-50 lg:border-none"><IconSwords /></span>
+        <span class="flex justify-center w-full p-2 lg:p-0 border-b border-b-primary lg:border-none"><IconSwords /></span>
       </div>
       <div
         v-for="{ username } in tournament?.players"
         :key="username"
-        class="inline-flex text-xl px-3 py-2 [&:not(:last-child)]:border-b border-slate-50"
+        class="inline-flex text-xl px-3 py-2 [&:not(:last-child)]:border-b border-b-primary"
       >
         {{ username }}
       </div>
     </div>
-    <div class="flex border border-slate-50 overflow-x-auto">
+    <div class="flex border border-primary overflow-x-auto">
       <div class="flex">
-        <div class="border border-slate-50">
-          <span class="inline-flex w-full justify-center border-b border-b-slate-50 py-[26px] lg:py-[11.5dpx] text-lg lg:text-base">
+        <div class="border border-primary">
+          <span class="inline-flex w-full justify-center border-b border-b-primary py-[26px] lg:py-[11.5dpx] text-lg lg:text-base">
             Střelec 1
           </span>
           <div
             v-for="{ scorerFirstName } in tournament?.players"
             :key="scorerFirstName"
-            class="[&:not(:last-child)]:border-b border-b-slate-50"
+            class="[&:not(:last-child)]:border-b border-b-primary"
           >
             <div class="text-center text-xl px-4 py-2 text-nowrap">
               {{ scorerFirstName }}
             </div>
           </div>
         </div>
-        <div class="border border-slate-50">
-          <span class="inline-flex w-full justify-center border-b border-b-slate-50 py-[26px] lg:py-[11.5px] text-lg lg:text-base">
+        <div class="border border-primary">
+          <span class="inline-flex w-full justify-center border-b border-b-primary py-[26px] lg:py-[11.5px] text-lg lg:text-base">
             Střelec 2
           </span>
           <div
             v-for="{ scorerSecondName } in tournament?.players"
             :key="scorerSecondName"
-            class="[&:not(:last-child)]:border-b border-b-slate-50"
+            class="[&:not(:last-child)]:border-b border-b-primary"
           >
-            <div class="[&:not(:last-child)]:border-b border-b-slate-50 text-center text-xl px-4 py-2 text-nowrap">
+            <div class="[&:not(:last-child)]:border-b border-b-primary text-center text-xl px-4 py-2 text-nowrap">
               {{ scorerSecondName }}
             </div>
           </div>
@@ -67,10 +67,10 @@ const numberOfMatches = tournament.value?.userMatches ? tournament.value?.userMa
         v-if="tournament!.userMatches.length > 0"
         v-for="col, row in Array.from({ length: numberOfMatches }, (_, index) => index)"
         :key="row"
-        class="[&:not(:last-child)]:border-r border-r-slate-50 [&:not(:last-child)]:border-b border-b-slate-50"
+        class="[&:not(:last-child)]:border-r border-r-primary [&:not(:last-child)]:border-b border-b-primary"
       >
-        <div class="flex flex-col lg:flex-row p-0 lg:p-3 gap-0 lg:gap-2">
-          <span class="p-2 lg:p-0 border-b border-b-slate-50 lg:border-none text-nowrap">
+        <div class="border-b border-primary flex flex-col lg:flex-row p-0 lg:p-3 gap-0 lg:gap-2">
+          <span class="p-2 lg:p-0 border-b border-b-primary lg:border-none text-nowrap">
             {{ tournament!.userMatches[row * numberOfPlayers].homeTeamName }}
           </span> 
           <span class="hidden lg:inline-block">-</span> 
