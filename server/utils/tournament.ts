179eb207fdb0d4ed57946dca10261c460f693116
Jiří Machynka <machynka.j@gmail.com>
2024-09-18 13:45:14 +0200

    might fix main table

--- server/utils/tournament.ts
+++ server/utils/tournament.ts
@@ -117,7 +117,7 @@ export const getAllTournamentData = async (userId: string, tournamentId: number)
 		.leftJoin(Tournaments, eq(TournamentMatchTips.tournamentId, Tournaments.id))
 		.leftJoin(homeTeam, eq(TournamentMatchTips.homeTeamId, homeTeam.id))
 		.leftJoin(awayTeam, eq(TournamentMatchTips.awayTeamId, awayTeam.id))
-		.where(and(eq(TournamentMatchTips.locked, true), eq(TournamentMatchTips.tournamentId, tournamentId)))
+		.where(and(eq(TournamentMatchTips.locked, true), eq(TournamentMatchTips.tournamentId, tournamentId), isNotNull(UserMatchTips.points)))
 		.orderBy(TournamentMatchTips.id, UserMatchTips.playerId);
 
 	return {
