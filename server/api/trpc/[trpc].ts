bd8abaf3f282c729af591f9d204a7df032e656d6
JiriMachynka <machynka.j@gmail.com>
2024-11-04 21:50:06 +0100

    1 minute SWR

--- server/api/trpc/[trpc].ts
+++ server/api/trpc/[trpc].ts
@@ -7,11 +7,11 @@ export default createNuxtApiHandler({
 	router: appRouter,
 	createContext,
 	responseMeta() {
-		const ONE_HOUR_IN_SECONDS = 60 * 60;
+		const ONE_MINUTE = 60;
 
 		return {
 			headers: {
-				'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_HOUR_IN_SECONDS}`,
+				'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_MINUTE}`,
 			},
 		};
 	},
