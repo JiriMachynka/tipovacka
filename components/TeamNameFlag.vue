a081f0844c09b548cbc9eb5879402f07bd1f7364
Jiří Machynka <machynka.j@gmail.com>
2024-11-29 21:24:07 +0100

    use NuxtImg

--- components/TeamNameFlag.vue
+++ components/TeamNameFlag.vue
@@ -9,7 +9,7 @@ const countries = ref(useTeams());
 </script>
 <template>
   <span class="inline-flex items-center gap-4">
-    <img v-if="props.showImg" :src="`/teams${countries.find((c) => c.name === props.teamName)?.code}.png`" class="h-6 w-6" />
+    <NuxtImg v-if="props.showImg" preload :src="`/teams${countries.find((c) => c.name === props.teamName)?.code}.png`" class="h-6 w-6" />
     <span v-else :class="`text-lg fi fi-${countries.find((c) => c.name === props.teamName)?.code}`" />
     <span>{{ teamName }}</span>
   </span>
