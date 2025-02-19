7f01831bb8d47871c58e454fe02558c2d23bc00d
JiriMachynka <machynka.j@gmail.com>
2025-02-19 12:54:33 +0100

    Minor formatting and whitespace cleanup in point calculation function

--- server/utils/calculatePoints.ts
+++ server/utils/calculatePoints.ts
@@ -1,8 +1,8 @@
 export const calculatePoints = (userHomeScore: number, userAwayScore: number, homeScore: number, awayScore: number) => {
 	if (userHomeScore === homeScore && userAwayScore === awayScore) return 3;
-	
+
 	const predictedOutcome = Math.sign(userHomeScore - userAwayScore);
 	const actualOutcome = Math.sign(homeScore - awayScore);
-	
+
 	return predictedOutcome === actualOutcome ? 1 : 0;
 };
