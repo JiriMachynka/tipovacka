604199099f049a0f3474e9cd31a9609a66b5ab6c
JiriMachynka <machynka.j@gmail.com>
2025-02-19 12:52:06 +0100

    Simplify point calculation logic for match predictions

--- server/utils/calculatePoints.ts
+++ server/utils/calculatePoints.ts
@@ -1,13 +1,8 @@
 export const calculatePoints = (userHomeScore: number, userAwayScore: number, homeScore: number, awayScore: number) => {
-	const draw = userHomeScore === userAwayScore && homeScore === awayScore;
-	const homeWin = userHomeScore > userAwayScore && homeScore > awayScore;
-	const awayWin = userHomeScore < userAwayScore && homeScore < awayScore;
-
-	const exactDraw = userHomeScore === homeScore && userHomeScore === userAwayScore && homeScore === awayScore;
-	const exactHomeWin = userHomeScore === homeScore && userAwayScore === awayScore && homeScore > awayScore;
-	const exactAwayWin = userHomeScore === homeScore && userAwayScore === awayScore && homeScore < awayScore;
-
-	if (exactDraw || exactHomeWin || exactAwayWin) return 3;
-	if (draw || homeWin || awayWin) return 1;
-	return 0;
+	if (userHomeScore === homeScore && userAwayScore === awayScore) return 3;
+	
+	const predictedOutcome = Math.sign(userHomeScore - userAwayScore);
+	const actualOutcome = Math.sign(homeScore - awayScore);
+	
+	return predictedOutcome === actualOutcome ? 1 : 0;
 };
