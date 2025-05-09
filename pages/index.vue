c59bb600afb6200d2e9644ad28afa3d3e847ecaa
JiriMachynka <machynka.j@gmail.com>
2025-05-09 16:19:03 +0200

    Add middleware to redirect authenticated users from home to tournaments page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
+<script lang="ts" setup>
+definePageMeta({
+	middleware: [
+		(to) => {
+			const session = useSupabaseSession();
+
+			if (session.value && to.path === '/') {
+				return navigateTo('/tournaments', {
+					replace: true,
+					redirectCode: 302,
+				});
+			}
+		},
+	],
+});
+</script>
 <template>
   <div class="flex flex-col justify-center items-center h-[100dvh]">
     <Button class="text-4xl px-7 py-5" as-child>
